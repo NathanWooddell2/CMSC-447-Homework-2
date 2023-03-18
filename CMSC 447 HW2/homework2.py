cfc004d1cd4abd25a44944c390d32c75d85aa256
NathanWooddell2 <Nathanw2@umbc.edu>
2023-03-17 21:03:16 -0400

    Flask app progress
    Worked on declarations for flask functions, and a bunch of placeholder code

--- CMSC 447 HW2/homework2.py
+++ CMSC 447 HW2/homework2.py
@@ -4,9 +4,32 @@
 # Description : This is the flask integration between my website (as defined in the Homepage.html file) and the 
 
 # I chose to use sqlite3, and flask as sqlite3 was included in python after Python(2.5).
-from flask import flask
+from flask import Flask, render_template 
 import sqlite3
 
-# A testing main for my python code to determine whether flask is working.
-if __name__ == "__main__":
-    print("Welcome home")
+backend = Flask(__name__)
+
+# Establishes a conncetion to the database created by the Create_Database.py file
+def resolve_db():
+    database = sqlite3.connect('homework2.db')
+    database.row_factory = sqlite3.Row
+    return database
+
+# A Function to display information on a student based on name
+@backend.route('/')
+def get_student_from_db():
+    database = resolve_db()
+    # currently this just returns everything from the gradebook -> needs work
+    gradebook = database.execute('SELECT * FROM gradebook').fetchall() # GIVE THIS A BETTER NAME
+    database.close()
+    return render_template('homepage.html', gradebook=gradebook)
+
+# A function to delete a selected student based on a provided ID
+@backend.route('/')
+def remove_student_from_db():
+    print("Placeholder")
+
+# A funtion to add a student to the database, must provide all information
+@backend.route('/')
+def add_student_to_db():
+    print("Placeholder")
