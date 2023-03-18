0bd0e58ca9a755187d25213e376225b55e4d15a6
NathanWooddell2 <Nathanw2@umbc.edu>
2023-03-17 20:37:18 -0400

    Created the Database
    Created the .db file, as it should exist when the assignment is turned in.

--- CMSC 447 HW2/Create_Database.py
+++ CMSC 447 HW2/Create_Database.py
@@ -6,11 +6,31 @@
 
 import sqlite3
 
+# Defines our database object, and creates it if it doesn't already exist
 database = sqlite3.connect("homework2.db")
 
+# Read from the SQL file, and execute the script present within
 with open('homework2.sql') as file:
     database.executescript(file.read())
 
-    cursor = database.cursor()
+# defines a cursor for the .sql file
+cursor = database.cursor()
 
-    cursor.execute()
+# All of the predefined people who need to be added to the database
+cursor.execute("INSERT INTO gradebook (StudentName, StudentID, StudentScore) VALUES ('Steve Smith', 211, 80)")
+
+cursor.execute("INSERT INTO gradebook (StudentName, StudentID, StudentScore) VALUES ('Jian Wong', 122, 92)")
+
+cursor.execute("INSERT INTO gradebook (StudentName, StudentID, StudentScore) VALUES ('Chris Peterson', 213, 91)")
+
+cursor.execute("INSERT INTO gradebook (StudentName, StudentID, StudentScore) VALUES ('Sai Patel', 524, 94)")
+
+cursor.execute("INSERT INTO gradebook (StudentName, StudentID, StudentScore) VALUES ('Andrew Whitehead', 425, 99)")
+
+cursor.execute("INSERT INTO gradebook (StudentName, StudentID, StudentScore) VALUES ('Lynn Roberts', 626, 90)")
+
+cursor.execute("INSERT INTO gradebook (StudentName, StudentID, StudentScore) VALUES ('Robert Sanders', 287, 75)")
+
+# Makes an initial commit 
+database.commit()
+database.close()
--- CMSC 447 HW2/homework2.py
+++ CMSC 447 HW2/homework2.py
@@ -1,14 +1,11 @@
-'''
-Name : Nathan Wooddell
-USERID : TC56612
-Project : CMSC 447 - Homework 2
-Description : This is the flask integration between my website (as defined in the Homepage.html file) and the 
-'''
+# Name : Nathan Wooddell
+# ID : TC56612
+# Project : CMSC 447 - Homework 2
+# Description : This is the flask integration between my website (as defined in the Homepage.html file) and the 
+
 # I chose to use sqlite3, and flask as sqlite3 was included in python after Python(2.5).
 from flask import flask
 import sqlite3
-
-
 
 # A testing main for my python code to determine whether flask is working.
 if __name__ == "__main__":
